0d2a8facff9caaab0ce1aedaed4e1e7346dbcb63
Rahul <rahul.sharma@thedriftpoint.com>
2024-10-02 16:31:47 +0530

    complete whole project

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
  useEffect(() => {
     if (authUser) {
-        const socket = io("http://localhost:5000", {
+        const socket = io("https://chatting-app-1xza.onrender.com", {
             query:{
                 userId: authUser._id,
             },
