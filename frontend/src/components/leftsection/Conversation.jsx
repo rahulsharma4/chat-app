2856666566f5eaaa1fed6574cabe416a687db89c
Rahul <rahul.sharma@thedriftpoint.com>
2024-10-16 15:24:16 +0530

    complete whole project

--- frontend/src/components/leftsection/Conversation.jsx
+++ frontend/src/components/leftsection/Conversation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import useConversation from '../../zustand/useConversation';
 import { useSocketContext } from '../../context/SocketContext';
 
-const Conversation = ({conversation, emoji}) => {
+const Conversation = ({conversation, emoji, lastIdx}) => {
  const {selectedConversation, setSelectedConversation} = useConversation()
 
  const isSelected = selectedConversation?._id === conversation._id;
@@ -27,7 +27,7 @@ const Conversation = ({conversation, emoji}) => {
       </div>
     </div>
 
-    {/* {!lastIdx && <div className='divider my-0 py-0 h-1'/>} */}
+    {!lastIdx && <div className='divider my-0 py-0 h-1'/>}
     </>
   )
 }
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
  useEffect(() => {
     if (authUser) {
-        const socket = io("https://chatting-app-1xza.onrender.com", {
+        const socket = io("https://chatting-app-1xza.onrender.com/", {
             query:{
                 userId: authUser._id,
             },
